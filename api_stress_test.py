1425f7591a454218b2a809bbc3c81e38312758c2
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2021-12-18 11:39:44 -0300

    Refatora código

--- api_stress_test.py
+++ api_stress_test.py
@@ -14,29 +14,28 @@ threads = []
 def criar_thread(name):
     thread_name = 'Thread {}'.format(name)
     threads.append(thread_name)
-    call_http_request()
+    realizar_chamada_http()
     threads.remove(thread_name)
 
 
-def call_http_request():
+def realizar_chamada_http():
     metodo = requisicao['metodo']
     url = requisicao['url']
     response = ''
-    start = time.process_time()
-    if (metodo == 'GET'):
+    tempo_inicial = time.process_time()
+    if metodo == 'GET':
         response = requests.get(
             requisicao['url'], headers=requisicao['headers'])
-    if (metodo == 'POST'):
-        response = requests.post(
-            requisicao['url'], headers=requisicao['headers'], data=requisicao['dados'])
-    response_time = time.process_time() - start
+    if metodo == 'POST':
+        response = requests.post(requisicao['url'], headers=requisicao['headers'], data=requisicao['dados'])
+    tempo_total = time.process_time() - tempo_inicial
     response_status = response.status_code
-    if (response_status >= 200 and response_status < 300):
+    if response_status >= 200 and response_status < 300:
         totais.append('Sucesso')
-        print('{} - {} - Resposta: {} - {}ms'.format(url, metodo, response_status, response_time))
+        print('{} - {} - Resposta: {} - {}ms'.format(url, metodo, response_status, tempo_total))
     else:
         totais.append('Falha')
-        print('{} - {} - Resposta: {} - {}ms - ERROR'.format(url, metodo, response_status, response_time))
+        print('{} - {} - Resposta: {} - {}ms - ERROR'.format(url, metodo, response_status, tempo_total))
     return response_status
 
 
